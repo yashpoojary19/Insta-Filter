b692caad72e30b9feaa50cc89a7788c76b027d2c
Yash Poojary <yashspoojary245@gmail.com>
2021-11-03 18:14:46 +0530

    Initial Commit

---
+++ Insta Filter/Insta_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Insta_FilterApp.swift
+//  Insta Filter
+//
+//  Created by Yash Poojary on 03/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Insta_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
