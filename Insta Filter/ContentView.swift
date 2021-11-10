e6fbe3cf59c614cfdf491b485661f8a85ba0bf16
Yash Poojary <yashspoojary245@gmail.com>
2021-11-10 19:33:00 +0530

    Up to using coordinates

--- Insta Filter/ContentView.swift
+++ Insta Filter/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
+        }
+        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: $inputImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+        
+
     }
 }
 
@@ -19,3 +45,128 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//struct ContentView: View {
+//    @State private var image: Image?
+//
+//
+//    var body: some View {
+//        VStack {
+//            image?
+//                .resizable()
+//                .scaledToFit()
+//        }
+//        .onAppear(perform: loadData)
+//    }
+//
+//
+//    func loadData() {
+//        guard let inputImage = UIImage(named: "Example") else {
+//            return
+//        }
+//
+//        let beginImage = CIImage(image: inputImage)
+//
+//        let context = CIContext()
+//
+//
+//        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else {
+//            return
+//        }
+//
+//        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+//        currentFilter.setValue(1000, forKey: kCIInputRadiusKey)
+//        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
+//
+//
+//
+//        guard let outputImage = currentFilter.outputImage else {
+//            return
+//        }
+//
+//        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+//            let uiimage = UIImage(cgImage: cgImage)
+//
+//            image = Image(uiImage: uiimage)
+//        }
+//
+//    }
+//}
+
+//struct ContentView: View {
+//
+//    @State private var showActionSheet = false
+//    @State private var backgroundColor = Color.white
+//
+//    var body: some View {
+//        Text("Hello World!")
+//            .frame(width: 300, height: 300)
+//            .background(backgroundColor)
+//            .onTapGesture {
+//                showActionSheet = true
+//            }
+//            .confirmationDialog(Text("Change Background"), isPresented: $showActionSheet, titleVisibility: .visible) {
+//                Button("Change Color to red") { backgroundColor = .red  }
+//                Button("Change Color to orange") { backgroundColor = .orange }
+//                Button("Delete", role: .destructive) {}
+//                Button("Cancel", role: .cancel) {}
+//
+//            }
+//
+//
+//    }
+//}
+
+
+
+//
+//struct ContentView: View {
+//
+//    @State private var blurAmount: CGFloat = 0 {
+//        didSet {
+//            print("The current value is \(blurAmount)")
+//        }
+//    }
+//
+//    var body: some View {
+//        let blur = Binding<CGFloat>(
+//            get: {
+//                blurAmount
+//            }, set: {
+//                blurAmount = $0
+//            }
+//        )
+//
+//
+//        VStack {
+//            Text("Hello World!")
+//                .blur(radius: blurAmount)
+//
+//            Slider(value: blur, in: 0...10)
+//                .padding()
+//
+//        }
+//
+//    }
+//}
---
+++ Insta Filter/ImagePicker.swift
@@ -0,0 +1,60 @@
+//
+//  ImagePicker.swift
+//  Insta Filter
+//
+//  Created by Yash Poojary on 10/11/21.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+  
+    
+    //delegates are objects which respond to events which occur elsewhere; Delegate are added to textfield, it's notified when there's any change, they
+    // are able to change stuff without having to create a custom text field of their own
+    
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+    }
+    
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+     
+    
+    
+    
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        
+        // so that whenever a user selects an image, it will report that action to coordinator.
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+}
