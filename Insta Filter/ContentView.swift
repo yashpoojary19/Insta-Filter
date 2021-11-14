29d5bd0fb7308bab93e130a54fe25bb34b9e7502
Yash Poojary <yashspoojary245@gmail.com>
2021-11-14 13:13:27 +0530

    Save images to gallery

--- Insta Filter/ContentView.swift
+++ Insta Filter/ContentView.swift
@@ -7,36 +7,52 @@
 
 import SwiftUI
 
-
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("save finished")
+    }
+}
 
 struct ContentView: View {
+    @State private var isShowing = false
     @State private var image: Image?
-    @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
-    
-    
-    
-    
+
+
+
     var body: some View {
         VStack {
             image?
                 .resizable()
                 .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
+
+            Button("Select an Image") {
+                isShowing = true
             }
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: $inputImage)
+        .sheet(isPresented: $isShowing, onDismiss: loadImage) {
+            ImagePicker(inputImage: $inputImage)
         }
+
     }
-    
+
     func loadImage() {
-        guard let inputImage = inputImage else { return }
+        guard let inputImage = inputImage else {
+            return
+        }
         image = Image(uiImage: inputImage)
         
-
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
+        
+        
+        
+       
+      
     }
 }
 
@@ -45,6 +61,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
 
 
 
--- Insta Filter/ImagePicker.swift
+++ Insta Filter/ImagePicker.swift
@@ -7,54 +7,138 @@
 
 import SwiftUI
 
+
 struct ImagePicker: UIViewControllerRepresentable {
-  
     
-    //delegates are objects which respond to events which occur elsewhere; Delegate are added to textfield, it's notified when there's any change, they
-    // are able to change stuff without having to create a custom text field of their own
     
     
     class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
         var parent: ImagePicker
+        
         
         init(_ parent: ImagePicker) {
             self.parent = parent
         }
         
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let uiImage = info[.originalImage] as? UIImage {
-                parent.image = uiImage
+            if let image = info[.originalImage] as? UIImage {
+                parent.inputImage = image
             }
             
             parent.presentationMode.wrappedValue.dismiss()
         }
-        
     }
     
-    @Binding var image: UIImage?
-    @Environment(\.presentationMode) var presentationMode
-    
+
     
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
-    
-     
-    
-    
+    @Binding var inputImage: UIImage?
+    @Environment(\.presentationMode) var presentationMode
     
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
-        
-        // so that whenever a user selects an image, it will report that action to coordinator.
         return picker
     }
     
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         
     }
-    
+   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//struct ImagePicker: UIViewControllerRepresentable {
+//
+//
+//    //delegates are objects which respond to events which occur elsewhere; Delegate are added to textfield, it's notified when there's any change, they
+//    // are able to change stuff without having to create a custom text field of their own
+//
+//
+//    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//        var parent: ImagePicker
+//
+//        init(_ parent: ImagePicker) {
+//            self.parent = parent
+//        }
+//
+//        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//            if let uiImage = info[.originalImage] as? UIImage {
+//                parent.image = uiImage
+//            }
+//
+//            parent.presentationMode.wrappedValue.dismiss()
+//        }
+//
+//    }
+//
+//    @Binding var image: UIImage?
+//    @Environment(\.presentationMode) var presentationMode
+//
+//
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(self)
+//    }
+//
+//
+//
+//
+//
+//
+//
+//    func makeUIViewController(context: Context) -> UIImagePickerController {
+//        let picker = UIImagePickerController()
+//        picker.delegate = context.coordinator
+//
+//        // so that whenever a user selects an image, it will report that action to coordinator.
+//        return picker
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+//
+//    }
+//
+//}
